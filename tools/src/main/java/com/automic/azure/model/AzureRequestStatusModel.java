4e3d0cd7dbf78596f321367163e2cb017ff20bc4
unknown <anuragupadhyay@Anurag4043.Nagarro.local>
2015-08-18 16:05:27 +0530

    PCK_AZURE-8<PCK_AZURE-62>:Check request status

--- tools/src/main/java/com/automic/azure/model/AzureRequestStatusModel.java
+++ tools/src/main/java/com/automic/azure/model/AzureRequestStatusModel.java
@@ -2,6 +2,7 @@ package com.automic.azure.model;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * An POJO java class which maps to XML structure which is required to handle the operation response.
  */
@@ -9,45 +10,45 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "Operation", namespace = "http://schemas.microsoft.com/windowsazure")
 public final class AzureRequestStatusModel {
 
-  private String requestTokenId;
-  private String requestStatus;
-  private String httpStatusCode;
-  private AzureErrorResponse error;
+    private String requestTokenId;
+    private String requestStatus;
+    private String httpStatusCode;
+    private AzureErrorResponse error;
 
-  @XmlElement(name = "ID", namespace = "http://schemas.microsoft.com/windowsazure")
-  public String getRequestTokenId() {
-    return requestTokenId;
-  }
+    @XmlElement(name = "ID", namespace = "http://schemas.microsoft.com/windowsazure")
+    public String getRequestTokenId() {
+        return requestTokenId;
+    }
 
-  public void setRequestTokenId(String requestTokenId) {
-    this.requestTokenId = requestTokenId;
-  }
+    public void setRequestTokenId(String requestTokenId) {
+        this.requestTokenId = requestTokenId;
+    }
 
-  @XmlElement(name = "Status", namespace = "http://schemas.microsoft.com/windowsazure")
-  public String getRequestStatus() {
-    return requestStatus;
-  }
+    @XmlElement(name = "Status", namespace = "http://schemas.microsoft.com/windowsazure")
+    public String getRequestStatus() {
+        return requestStatus;
+    }
 
-  public void setRequestStatus(String requestStatus) {
-    this.requestStatus = requestStatus;
-  }
+    public void setRequestStatus(String requestStatus) {
+        this.requestStatus = requestStatus;
+    }
 
-  @XmlElement(name = "HttpStatusCode", namespace = "http://schemas.microsoft.com/windowsazure")
-  public String getHttpStatusCode() {
-    return httpStatusCode;
-  }
+    @XmlElement(name = "HttpStatusCode", namespace = "http://schemas.microsoft.com/windowsazure")
+    public String getHttpStatusCode() {
+        return httpStatusCode;
+    }
 
-  public void setHttpStatusCode(String httpStatusCode) {
-    this.httpStatusCode = httpStatusCode;
-  }
+    public void setHttpStatusCode(String httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
 
-  @XmlElement(name = "Error", namespace = "http://schemas.microsoft.com/windowsazure")
-  public AzureErrorResponse getError() {
-    return error;
-  }
+    @XmlElement(name = "Error", namespace = "http://schemas.microsoft.com/windowsazure")
+    public AzureErrorResponse getError() {
+        return error;
+    }
 
-  public void setError(AzureErrorResponse error) {
-    this.error = error;
-  }
+    public void setError(AzureErrorResponse error) {
+        this.error = error;
+    }
 
 }
