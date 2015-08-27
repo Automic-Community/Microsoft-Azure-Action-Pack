f95e31166990e01cfe87b482ea074f2eec20ed1d
unknown <vivekmalhotra@vivek2702.Nagarro.local>
2015-08-27 11:55:45 +0530

    PCK_AZURE-10<PCK_AZURE-90> fixed checkstyle issues

--- tools/src/main/java/com/automic/azure/config/HttpClientConfig.java
+++ tools/src/main/java/com/automic/azure/config/HttpClientConfig.java
@@ -6,58 +6,57 @@ import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.client.urlconnection.HTTPSProperties;
 
 /**
- * This class is used to create HTTP Client using specified input parameters.
- * Subsequently client can be used to invoke HTTP operations on resources.
+ * This class is used to create HTTP Client using specified input parameters. Subsequently client can be used to invoke
+ * HTTP operations on resources.
  */
 
 public final class HttpClientConfig {
 
-	private HttpClientConfig() {
-	}
+    private HttpClientConfig() {
+    }
 
-	/**
-	 * Method to create HTTP client config with user defined ssl context
-	 * 
-	 * @param keyStore
-	 *            kesytore file
-	 * @param password
-	 *            password to keystore file
-	 * @param connectionTimeOut
-	 *            connection timeout
-	 * @param readTimeOut
-	 *            read timeout
-	 * @return
-	 * @throws AzureException
-	 */
-	public static ClientConfig getClientConfig(String keyStore, String password, int connectionTimeOut, 
-			int readTimeOut)
-			throws AzureException {
-		ClientConfig config = new DefaultClientConfig();
+    /**
+     * Method to create HTTP client config with user defined ssl context
+     * 
+     * @param keyStore
+     *            kesytore file
+     * @param password
+     *            password to keystore file
+     * @param connectionTimeOut
+     *            connection timeout
+     * @param readTimeOut
+     *            read timeout
+     * @return
+     * @throws AzureException
+     */
+    public static ClientConfig getClientConfig(String keyStore, String password, int connectionTimeOut, int readTimeOut)
+            throws AzureException {
+        ClientConfig config = new DefaultClientConfig();
 
-		config.getProperties().put(ClientConfig.PROPERTY_CONNECT_TIMEOUT, connectionTimeOut);
-		config.getProperties().put(ClientConfig.PROPERTY_READ_TIMEOUT, readTimeOut);
+        config.getProperties().put(ClientConfig.PROPERTY_CONNECT_TIMEOUT, connectionTimeOut);
+        config.getProperties().put(ClientConfig.PROPERTY_READ_TIMEOUT, readTimeOut);
 
-		if (keyStore != null && password != null) {
-			AzureCertificatesManagement acm = new AzureCertificatesManagement(keyStore, password);
+        if (keyStore != null && password != null) {
+            AzureCertificatesManagement acm = new AzureCertificatesManagement(keyStore, password);
 
-			HTTPSProperties props = new HTTPSProperties(acm.hostnameVerifier(), acm.getSslContext());
-			config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, props);
-		}
+            HTTPSProperties props = new HTTPSProperties(acm.hostnameVerifier(), acm.getSslContext());
+            config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, props);
+        }
 
-		return config;
-	}
+        return config;
+    }
 
-	/**
-	 * Method to create HTTP client config without user defined ssl context
-	 * 
-	 * @param connectionTimeOut
-	 * @param readTimeOut
-	 * @return
-	 * @throws AzureException
-	 */
-	public static ClientConfig getClientConfig(int connectionTimeOut, int readTimeOut) throws AzureException {
+    /**
+     * Method to create HTTP client config without user defined ssl context
+     * 
+     * @param connectionTimeOut
+     * @param readTimeOut
+     * @return
+     * @throws AzureException
+     */
+    public static ClientConfig getClientConfig(int connectionTimeOut, int readTimeOut) throws AzureException {
 
-		return getClientConfig(null, null, connectionTimeOut, readTimeOut);
-	}
+        return getClientConfig(null, null, connectionTimeOut, readTimeOut);
+    }
 
 }
--- tools/src/main/java/com/automic/azure/util/ByteWriter.java
+++ tools/src/main/java/com/automic/azure/util/ByteWriter.java
@@ -18,101 +18,101 @@ import com.automic.azure.exception.AzureException;
  */
 public class ByteWriter {
 
-	private static final Logger LOGGER = LogManager.getLogger(ByteWriter.class);
+    private static final Logger LOGGER = LogManager.getLogger(ByteWriter.class);
 
-	private BufferedOutputStream bos = null;
+    private BufferedOutputStream bos = null;
 
-	public ByteWriter(OutputStream output) {
-		bos = new BufferedOutputStream(output, Constants.IO_BUFFER_SIZE);
-	}
+    public ByteWriter(OutputStream output) {
+        bos = new BufferedOutputStream(output, Constants.IO_BUFFER_SIZE);
+    }
 
-	/**
-	 * Method to get associated output stream.
-	 */
-	public OutputStream getStream() {
-		return bos;
-	}
+    /**
+     * Method to get associated output stream.
+     */
+    public OutputStream getStream() {
+        return bos;
+    }
 
-	/**
-	 * Method to write bytes to Stream
-	 * 
-	 * @param bytes
-	 * @throws AzureException
-	 */
-	public void write(byte[] bytes) throws AzureException {
-		write(bytes, 0, bytes.length);
-	}
+    /**
+     * Method to write bytes to Stream
+     * 
+     * @param bytes
+     * @throws AzureException
+     */
+    public void write(byte[] bytes) throws AzureException {
+        write(bytes, 0, bytes.length);
+    }
 
-	/**
-	 * Method to write specific part of byte array to Stream
-	 * 
-	 * @param bytes
-	 * @param offset
-	 * @param length
-	 * @throws AzureException
-	 */
-	public void write(byte[] bytes, int offset, int length) throws AzureException {
-		try {
-			bos.write(bytes, offset, length);
-		} catch (IOException e) {
-			LOGGER.error(ExceptionConstants.UNABLE_TO_WRITEFILE, e);
-			throw new AzureException(ExceptionConstants.UNABLE_TO_WRITEFILE, e);
-		}
-	}
+    /**
+     * Method to write specific part of byte array to Stream
+     * 
+     * @param bytes
+     * @param offset
+     * @param length
+     * @throws AzureException
+     */
+    public void write(byte[] bytes, int offset, int length) throws AzureException {
+        try {
+            bos.write(bytes, offset, length);
+        } catch (IOException e) {
+            LOGGER.error(ExceptionConstants.UNABLE_TO_WRITEFILE, e);
+            throw new AzureException(ExceptionConstants.UNABLE_TO_WRITEFILE, e);
+        }
+    }
 
-	/**
-	 * Method to write a String to stream
-	 * 
-	 * @param field
-	 * @throws AzureException
-	 */
-	public void write(String field) throws AzureException {
-		write(field.getBytes());
-	}
+    /**
+     * Method to write a String to stream
+     * 
+     * @param field
+     * @throws AzureException
+     */
+    public void write(String field) throws AzureException {
+        write(field.getBytes());
+    }
 
-	/**
-	 * Method to write a new line character to stream
-	 * 
-	 * @throws AzureException
-	 */
-	public void writeNewLine() throws AzureException {
-		write(System.lineSeparator());
-	}
+    /**
+     * Method to write a new line character to stream
+     * 
+     * @throws AzureException
+     */
+    public void writeNewLine() throws AzureException {
+        write(System.lineSeparator());
+    }
 
-	/**
-	 * Close the underlying stream
-	 * 
-	 * @throws AzureException
-	 */
-	public void close() throws AzureException {
-		try {
-			if (bos != null) {
-				bos.close();
-			} else {
-				LOGGER.error("Stream null!! Unable to close stream");
-				throw new AzureException(ExceptionConstants.UNABLE_TO_CLOSE_STREAM);
-			}
+    /**
+     * Close the underlying stream
+     * 
+     * @throws AzureException
+     */
+    public void close() throws AzureException {
+        try {
+            if (bos != null) {
+                bos.close();
+            } else {
+                LOGGER.error("Stream null!! Unable to close stream");
+                throw new AzureException(ExceptionConstants.UNABLE_TO_CLOSE_STREAM);
+            }
 
-		} catch (IOException e) {
-			LOGGER.error(ExceptionConstants.UNABLE_TO_CLOSE_STREAM, e);
-			throw new AzureException(ExceptionConstants.UNABLE_TO_CLOSE_STREAM, e);
-		}
-	}
+        } catch (IOException e) {
+            LOGGER.error(ExceptionConstants.UNABLE_TO_CLOSE_STREAM, e);
+            throw new AzureException(ExceptionConstants.UNABLE_TO_CLOSE_STREAM, e);
+        }
+    }
 
-	/**
-	 * Method to flush to stream
-	 * 
-	 * @throws AzureException
-	 */
-	public void flush() throws AzureException {
-		if (bos != null) {
-			try {
-				bos.flush();
-			} catch (IOException e) {
-				LOGGER.error(ExceptionConstants.UNABLE_TO_FLUSH_STREAM, e);
-				throw new AzureException(ExceptionConstants.UNABLE_TO_FLUSH_STREAM, e);
-			}
-		}
-	}
+    /**
+     * Method to flush to stream
+     * 
+     * @throws AzureException
+     */
+    public void flush() throws AzureException {
+        if (bos != null) {
+            try {
+                bos.flush();
+            } catch (IOException e) {
+                LOGGER.error(ExceptionConstants.UNABLE_TO_FLUSH_STREAM, e);
+                throw new AzureException(ExceptionConstants.UNABLE_TO_FLUSH_STREAM, e);
+            }
+        }
+    }
 
 }
